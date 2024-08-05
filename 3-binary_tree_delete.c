e9c545183f2079af940e6fe70fce160800487d78
Basco <martinez.xabi@laposte.net>
2024-08-05 11:36:06 +0000

    Reset tree

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -11,12 +11,10 @@
 
 void binary_tree_delete(binary_tree_t *tree);
 {
-	binary_tree_t *tmp;
-
 	if (tree == NULL)
-		return;
+		return;	
 
-	
-
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
 	free(tree);
 }
